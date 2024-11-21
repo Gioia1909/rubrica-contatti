80a3cb81918a023e522e8cbc065ab031c92c6685
deboravillano <d.villano4@studenti.unisa.it>
2024-11-21 10:32:13 +0100

    Commit progetto - Debora ha scritto "Ciao sono Debbi"

--- src/main/java/it/university/group9/App.java
+++ src/main/java/it/university/group9/App.java
@@ -4,11 +4,11 @@ package it.university.group9;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "ciao scrivo questa cosa" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("ciao scrivo questa cosa");
         System.out.println("ARia mi vedi?");
+        System.out.println("Ciao sono debbi");
     }
 }
