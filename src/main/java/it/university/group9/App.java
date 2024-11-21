aec4822919155ba399c6f40337f719e8fe2c3e48
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-11-21 10:25:13 +0100

    commit arianna

--- src/main/java/it/university/group9/App.java
+++ src/main/java/it/university/group9/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "ciao scrivo questa cosa" );
+        System.out.println("ARia mi vedi?");
     }
 }
