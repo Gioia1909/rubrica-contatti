bc36a351371082d5481b71146b4e4721ae17a699
ariannapal <ari19arianna@gmail.com>
2024-12-02 17:04:44 +0100

    definizione classi

---
+++ src/main/java/it/university/group9/rubricacontattigroup9/Rubrica.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.university.group9.rubricacontattigroup9;
+
+/**
+ *
+ * @author ari19
+ */
+public class Rubrica {
+    
+    
+}
