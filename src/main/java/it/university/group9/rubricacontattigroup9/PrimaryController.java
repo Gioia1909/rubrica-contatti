2262126bdedb0a137843d6156cc6bb9231d329fc
ari19 <ari19@LAPTOP-Q88UN02U.station>
2024-12-02 14:03:21 +0100

    scheletro  interfaccia utente 2

--- src/main/java/it/university/group9/rubricacontattigroup9/PrimaryController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/PrimaryController.java
@@ -5,8 +5,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class PrimaryController implements Initializable {
+
+    @FXML
+    private Button primaryButton;
+    private TextField textfield;
 
     @FXML
     private void switchToSecondary() throws IOException {
