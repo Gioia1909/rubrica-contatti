3fa046a1d7a8e4047cc180d980b493c86be82391
ariannapal <ari19arianna@gmail.com>
2024-12-02 18:50:22 +0100

    classe contatto: attributi e costruttore

--- src/main/java/it/university/group9/rubricacontattigroup9/Contatto.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/Contatto.java
@@ -4,10 +4,26 @@
  */
 package it.university.group9.rubricacontattigroup9;
 
+import java.util.List;
+
 /**
  *
  * @author ari19
  */
 public class Contatto {
-    
+
+    private String nome;
+    private String cognome;
+    private List<String> numeri;
+    private List<String> email;
+    private String note;
+
+    public Contatto(String nome, String cognome, List<String> numeri, List<String> emails, String note) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeri = numeri;
+        this.email = new ArrayList<>();
+        this.note = new ArrayList<>();
+    }
+
 }
