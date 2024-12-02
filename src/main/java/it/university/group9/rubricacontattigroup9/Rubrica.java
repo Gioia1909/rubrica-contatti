7b79b4bafec81a7343a462114a4a31845e3384ed
ariannapal <ari19arianna@gmail.com>
2024-12-02 18:26:05 +0100

    Definizione interfacce e classi

--- src/main/java/it/university/group9/rubricacontattigroup9/Rubrica.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/Rubrica.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package it.university.group9.rubricacontattigroup9;
 
@@ -8,7 +8,6 @@ package it.university.group9.rubricacontattigroup9;
  *
  * @author ari19
  */
-public class Rubrica {
-    
+public interface Rubrica {
     
 }
