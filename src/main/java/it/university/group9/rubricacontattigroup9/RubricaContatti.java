fd8dfbc459b28e27297634df750263ff713a9b1e
deboravillano <d.villano4@studenti.unisa.it>
2024-12-02 19:06:24 +0100

    Inizio implementazione classe RubricaContatti

--- src/main/java/it/university/group9/rubricacontattigroup9/RubricaContatti.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/RubricaContatti.java
@@ -4,11 +4,24 @@
  */
 package it.university.group9.rubricacontattigroup9;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author ari19
  */
 public class RubricaContatti implements Rubrica {
-    
-    
+
+    private List<Contatto> contatti;
+
+    public RubricaContatti(List<Contatto> contatti) {
+        this.contatti = new LinkedList<>();
+    }
+
+    @Override
+    public void aggiungiContatto(Contatto contatto) {
+        contatti.add(contatto);
+    }
+
 }
