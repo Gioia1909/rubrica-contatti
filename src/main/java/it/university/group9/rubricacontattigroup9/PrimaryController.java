dcd917ffde82eb3779dbe9ed4cb9eabb894da74b
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-12-02 13:14:29 +0100

    InterfacciaUtente

--- src/main/java/it/university/group9/rubricacontattigroup9/PrimaryController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/PrimaryController.java
@@ -1,12 +1,20 @@
 package it.university.group9.rubricacontattigroup9;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
