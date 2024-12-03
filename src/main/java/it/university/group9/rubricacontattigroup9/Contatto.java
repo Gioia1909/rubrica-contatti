99b11c0b9821d7693fec90cfa50644d18a682f41
monto <monto@LAPTOP-BA2GGMI7.Home>
2024-12-03 19:09:00 +0100

    Documentazione della classe Contatto

--- src/main/java/it/university/group9/rubricacontattigroup9/Contatto.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/Contatto.java
@@ -8,8 +8,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- *
- * @author ari19
+ * @file Contatto.java
+ * @brief Classe che rappresenta un contatto con un nome, un cognome, da uno a tre numeri di telefono, da una a tre email e delle note.
+ * 
+ * Questa classe è utilizzata per gestire i dati di un contatto in una rubrica.
+ * Ogni contatto ha un nome, un cognome, una lista di numeri di telefono, una lista di email e delle note.
+ * 
+ * @author Gruppo09
+ * @date 03/12/2024
+ * 
  */
 public class Contatto {
 
@@ -18,7 +25,17 @@ public class Contatto {
     private List<String> numeri;
     private List<String> emails;
     private String note;
-
+/**
+     * @brief Costruttore della classe Contatto.
+     * 
+     * Crea un'istanza della classe Contatto con i parametri forniti.
+     * 
+     * @param[in] nome Il nome del contatto.
+     * @param[in] cognome Il cognome del contatto.
+     * @param[in] numeri Lista di numeri di telefono associati al contatto.
+     * @param[in] emails Lista di email associate al contatto.
+     * @param[in] note Note aggiuntive relative al contatto.
+     */
     public Contatto(String nome, String cognome, List<String> numeri, List<String> emails, String note) {
         this.nome = nome;
         this.cognome = cognome;
@@ -27,27 +44,60 @@ public class Contatto {
         this.note = note;
     }
     
-    
+    /**
+     * @brief Metodo che restituisce il nome del contatto.
+     * 
+     * @return Nome del contatto.
+     */
     public String getNome(){
         return nome;
     }
     
+    
+    /**
+     * @brief Metodo che restituisce il cognome del contatto.
+     * 
+     * @return Cognome del contatto.
+     */
     public String getCognome(){
         return cognome;
     }
     
+      /**
+     * @brief Metodo che resituisce la lista dei numeri di telefono del contatto.
+     * 
+     * @return Una lista contenente i numeri di telefono del contatto.
+     */
     public List<String> getNumeri(){
         return numeri;
     }
+
+ /**
+     * @brief Metodo che restituisce la lista delle email del contatto.
+     * 
+     * @return Una lista contenente le email del contatto.
+     */
     
     public List<String> getEmails(){
         return emails;
     }
     
+    
+    /**
+     * @brief Metodo che restituisce le note del contatto.
+     * 
+     * @return Le note associate al contatto.
+     */
     public String getNote(){
         return note;
     }
     
+    
+    /**
+     * @brief Restituisce i dati del contatto tramite
+     * 
+     * @return Dati del contatto: nome, cognome, numeri di telefono, email e note.
+     */
     @Override
     public String toString(){
         StringBuffer sb= new StringBuffer();
