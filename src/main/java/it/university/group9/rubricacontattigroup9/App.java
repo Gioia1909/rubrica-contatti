5188be3c66fa43629c7e5b405a40f7749529ae26
ariannapal <ari19arianna@gmail.com>
2024-12-03 21:40:05 +0100

    aggiunta titolo

--- src/main/java/it/university/group9/rubricacontattigroup9/App.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("InterfacciaUtente"), 800, 580);
+        stage.setTitle("Rubrica Contatti");
         stage.setScene(scene);
         stage.show();
     }
