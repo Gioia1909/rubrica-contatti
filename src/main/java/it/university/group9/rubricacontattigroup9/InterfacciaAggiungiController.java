3f65279c3676cdb4503b8790acdbf4e0f9d30506
ariannapal <ari19arianna@gmail.com>
2024-12-04 18:04:45 +0100

    aggiunte

--- src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaAggiungiController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaAggiungiController.java
@@ -4,9 +4,14 @@
  */
 package it.university.group9.rubricacontattigroup9;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,9 +20,62 @@ import javafx.fxml.Initializable;
  */
 public class InterfacciaAggiungiController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    private InterfacciaUtenteController interfacciaUtenteController;  // Riferimento al controller di InterfacciaUtente
+    
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField surnameField;
+
+    @FXML
+    private TextField email1Field;
+
+    @FXML
+    private TextField email2Field;
+
+    @FXML
+    private TextField email3Field;
+
+    @FXML
+    private TextField number1Field;
+
+    @FXML
+    private TextField number2Field;
+
+    @FXML
+    private TextField number3Field;
+    
+      @FXML
+    private TextField noteField;
+    
+    
+       public void setInterfacciaUtenteController(InterfacciaUtenteController controller) {
+        this.interfacciaUtenteController = controller;
+    }
+      
+      @FXML
+    void switchToInterfaccia(MouseEvent event) {
+         App.setRoot("InterfacciaUtente");
+
+    }
+ 
+     
+    @FXML
+    void addContact(MouseEvent event) {
+          App.setRoot("InterfacciaUtente");
+    }
+       
+       
+       
+       
+       
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
