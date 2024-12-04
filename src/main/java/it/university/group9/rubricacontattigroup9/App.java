68ee2d9514373044f3a7252bf5997804dfc3d573
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-12-04 10:35:24 +0100

    Modifiche pull

--- src/main/java/it/university/group9/rubricacontattigroup9/App.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("InterfacciaUtente"), 640, 480);
+        scene = new Scene(loadFXML("InterfacciaUtente"), 800, 580);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaUtenteController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaUtenteController.java
@@ -1,24 +1,44 @@
 package it.university.group9.rubricacontattigroup9;
 
+import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class InterfacciaUtenteController implements Initializable {
+    
+    
+    
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private ListView<String> myListView;
 
     @FXML
     private Button primaryButton;
-    private TextField textf;
-    
+
+    @FXML
+    private TextField textBar;
+
     @FXML
     private Button searchButton;
+    
+      
     @FXML
-    private Button deleteButton;
+    public void addName(MouseEvent event) {
+        myListView.getItems().add(textBar.getText());
+    }
 
+    
+   
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
@@ -31,5 +51,12 @@ public void initialize(URL location, ResourceBundle resources) {
     }
     
     
+    
+
+
 }
+
 }
+
+
+
