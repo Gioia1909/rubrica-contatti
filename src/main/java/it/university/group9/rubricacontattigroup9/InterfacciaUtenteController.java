7b9c30edefde969b758b965b5f558378841dbcba
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-12-04 11:39:35 +0100

    Risolto conflitto in InterfacciaUtenteController.java

--- src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaUtenteController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/InterfacciaUtenteController.java
@@ -10,19 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class InterfacciaUtenteController implements Initializable {
-    
-    
-    
+
     @FXML
     private Button addButton;
-    
+
     @FXML
     private Button deleteButton;
-    
+
     @FXML
     private Button searchButton;
 
@@ -35,44 +34,54 @@ public class InterfacciaUtenteController implements Initializable {
     @FXML
     private TextField textBar;
 
-      
-  //metodo provvisorio per vedere la listView
+    @FXML
+    private Label nameField;
+
+    @FXML
+    private Label surnameField;
+
+    @FXML
+    private Label email1Field;
+
+    @FXML
+    private Label email2Field;
+
+    @FXML
+    private Label email3Field;
+
+    @FXML
+    private Label number1Field;
+
+    @FXML
+    private Label number2Field;
+
+    @FXML
+    private Label number3Field;
+
+    //metodo provvisorio per vedere la listView
     @FXML
     public void addContact(MouseEvent event) {
         myListView.getItems().add(textBar.getText());
     }
-    
+
     //metodo provvisorio per vedere la listView
     @FXML
     public void deleteContact(MouseEvent event) {
-        int selezionato=myListView.getSelectionModel().getSelectedIndex();
+        int selezionato = myListView.getSelectionModel().getSelectedIndex();
         myListView.getItems().remove(selezionato);
     }
 
-    
-   
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
     @Override
-public void initialize(URL location, ResourceBundle resources) {
-    if (searchButton != null && searchButton.getScene() != null) {
-        searchButton.getScene().getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-    }
-    
-    
-    
+    public void initialize(URL location, ResourceBundle resources) {
+        if (searchButton != null && searchButton.getScene() != null) {
+            searchButton.getScene().getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        }
 
-
-}
-
-    @FXML
-    private void searchAction(ActionEvent event) {
     }
 
 }
-
-
-
