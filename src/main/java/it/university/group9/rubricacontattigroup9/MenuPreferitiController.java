d49280c52e7347704186c09559cb3749e62f8f85
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-12-04 10:59:12 +0100

    Ulteriori modifiche

--- src/main/java/it/university/group9/rubricacontattigroup9/MenuPreferitiController.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/MenuPreferitiController.java
@@ -1,12 +1,40 @@
 package it.university.group9.rubricacontattigroup9;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
-public class SecondaryController {
+public class MenuPreferitiController implements Initializable {
+
+    @FXML
+    private TextArea scrittaPreferiti;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button addPrefButton;
+    @FXML
+    private Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @FXML
+    private void editAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void addPrefButton(ActionEvent event) {
     }
 }
