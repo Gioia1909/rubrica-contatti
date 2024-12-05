10cbd3dc1278a0368734eb3489e1f00354cacf47
deboravillano <d.villano4@studenti.unisa.it>
2024-12-05 10:55:24 +0100

    Aggiunta la classe NumeroNonValidoException per gestire errori di validazione numero di telefono. - Creata la classe NumeroNonValidoException per rappresentare eccezioni legate alla validazione dei numeri  di telefono. - Inserita la classe nel package it.university.group9.rubricacontattigroup9.exceptions.

---
+++ src/main/java/it/university/group9/rubricacontattigroup9/exceptions/NumeroNonValidoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package it.university.group9.rubricacontattigroup9.exceptions;
+
+/**
+ *
+ * @author Gruppo09
+ */
+public class NumeroNonValidoException extends Exception {
+    public NumeroNonValidoException(String message) {
+        super(message);
+    }
+}
