2e849ff5e12db83d7b36751f0112294e335d6a83
deboravillano <d.villano4@studenti.unisa.it>
2024-12-05 10:54:25 +0100

    Aggiunta la classe EmailNonValidaException per gestire errori di validazione email. - Creata la classe EmailNonValidaException per rappresentare eccezioni legate alla validazione delle email. - Inserita la classe nel package it.university.group9.rubricacontattigroup9.exceptions.

---
+++ src/main/java/it/university/group9/rubricacontattigroup9/exceptions/EmailNonValidaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package it.university.group9.rubricacontattigroup9.exceptions;
+
+/**
+ *
+ * @author Gruppo09
+ */
+public class EmailNonValidaException extends Exception {
+    public EmailNonValidaException(String message) {
+        super(message);
+    }
+}
