fb81e343ef6b53f362c7a0682cd389e6e622c8f5
deboravillano <d.villano4@studenti.unisa.it>
2024-12-05 11:03:34 +0100

    Aggiunta la classe NomeNonValido per gestire errori di validazione del nome

---
+++ src/main/java/it/university/group9/rubricacontattigroup9/exceptions/NomeNonValidoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package it.university.group9.rubricacontattigroup9.exceptions;
+
+/**
+ *
+ * @author Gruppo09
+ */
+public class NomeNonValidoException extends Exception {
+    public NomeNonValidoException(String message) {
+        super(message);
+    }
+}
