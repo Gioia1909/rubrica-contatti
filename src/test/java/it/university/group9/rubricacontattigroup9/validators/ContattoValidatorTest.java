4678ea15b393c4e7e5ddf86c8fa1ccf3694c0262
deboravillano <d.villano4@studenti.unisa.it>
2024-12-10 19:22:22 +0100

    Implementata classe per il test validator

--- src/test/java/it/university/group9/rubricacontattigroup9/validators/ContattoValidatorTest.java
+++ src/test/java/it/university/group9/rubricacontattigroup9/validators/ContattoValidatorTest.java
@@ -1,12 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package it.university.group9.rubricacontattigroup9.validators;
 
-import it.university.group9.rubricacontattigroup9.Contatto;
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author imacpro
+ * @author deboravillano
  */
 public class ContattoValidatorTest {
     
@@ -39,50 +36,9 @@ public class ContattoValidatorTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of isNumeroDuplicato method, of class ContattoValidator.
-     */
-    @Test
-    public void testIsNumeroDuplicato() {
-        System.out.println("isNumeroDuplicato");
-        List<Contatto> contatti = null;
-        String numero = "";
-        boolean expResult = false;
-        boolean result = ContattoValidator.isNumeroDuplicato(contatti, numero);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isContattoDuplicato method, of class ContattoValidator.
-     */
-    @Test
-    public void testIsContattoDuplicato() {
-        System.out.println("isContattoDuplicato");
-        List<Contatto> contatti = null;
-        String nome = "";
-        String cognome = "";
-        boolean expResult = false;
-        boolean result = ContattoValidator.isContattoDuplicato(contatti, nome, cognome);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isEmailDuplicata method, of class ContattoValidator.
-     */
-    @Test
-    public void testIsEmailDuplicata() {
-        System.out.println("isEmailDuplicata");
-        List<Contatto> contatti = null;
-        String email = "";
-        boolean expResult = false;
-        boolean result = ContattoValidator.isEmailDuplicata(contatti, email);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
 }
