d9826ae7242771b266646fc03e8af405589a93a6
deboravillano <d.villano4@studenti.unisa.it>
2024-12-11 14:34:40 +0100

    Interfaccia AddressBookManager

---
+++ src/main/java/it/university/group9/rubricacontattigroup9/AddressBookManager.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.university.group9.rubricacontattigroup9;
+
+import javafx.event.ActionEvent;
+
+/**
+ *
+ * @author deboravillano
+ */
+public interface AddressBookManager {
+    public void deleteAction(ActionEvent event);
+
+    public void addAction(ActionEvent event); 
+
+    public void editAction(ActionEvent event);
+
+    public void searchAction(ActionEvent event);
+
+}
