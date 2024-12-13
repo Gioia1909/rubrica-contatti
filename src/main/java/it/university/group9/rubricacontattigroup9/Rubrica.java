f8f7b5b226177559c27d60585b0c0a3bd1d3ce00
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2024-12-13 12:37:02 +0100

    Fixing

--- src/main/java/it/university/group9/rubricacontattigroup9/Rubrica.java
+++ src/main/java/it/university/group9/rubricacontattigroup9/Rubrica.java
@@ -1,48 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package it.university.group9.rubricacontattigroup9;
 
 import it.university.group9.rubricacontattigroup9.InputOutput.SalvaCaricaRubrica;
-import it.university.group9.rubricacontattigroup9.exceptions.*;
-import it.university.group9.rubricacontattigroup9.validators.*;
-import java.io.IOException;
-import java.util.List;
+import it.university.group9.rubricacontattigroup9.InputOutput.SalvaCaricaPreferiti;
+import it.university.group9.rubricacontattigroup9.exceptions.CampoNonValidoException;
+import it.university.group9.rubricacontattigroup9.validators.ContattoValidator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-/**
- *
- * @author ari19
- */
-public class Rubrica implements GestioneRubrica {
+import java.io.IOException;
+import java.util.List;
+
+public class Rubrica {
 
     private ObservableList<Contatto> contactList;
-    
+    private ObservableList<Contatto> favoriteList;
 
     public Rubrica() {
-        this.contactList = FXCollections.observableArrayList();
+        this.contactList = SalvaCaricaRubrica.loadAddressBook();
+        this.favoriteList = SalvaCaricaPreferiti.loadFavoritesAddressBook();
+        synchronizeFavorites();
     }
 
-    @Override
-    public ObservableList<Contatto> getContact() {
+    /**
+     * @brief Sincronizza la lista dei preferiti con i contatti principali.
+     *
+     * Se un contatto nella lista dei preferiti non è più presente nella rubrica principale,
+     * viene rimosso automaticamente dai preferiti.
+     */
+    private void synchronizeFavorites() {
+        favoriteList.removeIf(fav -> !contactList.contains(fav));
+    }
+
+    /**
+     * @brief Restituisce la lista dei contatti.
+     * @return Lista osservabile dei contatti principali.
+     */
+    public ObservableList<Contatto> getContactList() {
         return contactList;
     }
-    
+
     /**
-     * @brief Aggiunge un contatto alla rubrica.
-     * 
-     * @param name Nome del contatto.
-     * @param surname Cognome del contatto.
-     * @param numbers Lista di numeri di telefono del contatto.
-     * @param emails Lista di email del contatto.
-     * @param note Note associate al contatto.
+     * @brief Restituisce la lista dei contatti preferiti.
+     * @return Lista osservabile dei contatti preferiti.
+     */
+    public ObservableList<Contatto> getFavoriteList() {
+        return favoriteList;
+    }
+
+    /**
+     * @brief Aggiunge un contatto alla rubrica principale.
      * @throws CampoNonValidoException Se uno dei campi non è valido.
      * @throws IOException Se si verifica un errore durante il salvataggio.
      */
-
-    @Override
     public void addContact(String name, String surname, List<String> numbers, List<String> emails, String note)
             throws CampoNonValidoException, IOException {
 
@@ -66,18 +75,7 @@ public class Rubrica implements GestioneRubrica {
 
     /**
      * @brief Modifica un contatto esistente.
-     * 
-     * @param oldContact Il contatto da modificare.
-     * @param name Nuovo nome del contatto.
-     * @param surname Nuovo cognome del contatto.
-     * @param numbers Nuovi numeri di telefono del contatto.
-     * @param emails Nuove email del contatto.
-     * @param note Nuove note associate al contatto.
-     * @throws CampoNonValidoException Se uno dei campi non è valido.
-     * @throws IOException Se si verifica un errore durante il salvataggio.
      */
-    
-    @Override
     public void editContact(Contatto oldContact, String name, String surname, List<String> numbers, List<String> emails, String note)
             throws CampoNonValidoException, IOException {
 
@@ -95,22 +93,41 @@ public class Rubrica implements GestioneRubrica {
             Contatto updatedContact = new Contatto(name, surname, numbers, emails, note);
             contactList.set(index, updatedContact);
             saveContacts();
+            saveFavorites(); // Aggiorna anche il file dei preferiti
         }
     }
-    /**
-     * @brief Rimuove un contatto dalla rubrica.
-     * 
-     * @param contact Il contatto da rimuovere.
-     * @throws IOException Se si verifica un errore durante il salvataggio.
-     */
-    @Override
-    public void deleteContact(Contatto contact) throws IOException{
-        contactList.remove(contact);
-        saveContacts();
-    }
-    
 
-    @Override
+    /**
+     * @brief Rimuove un contatto dalla rubrica principale e dai preferiti.
+     */
+    public void deleteContact(Contatto contact) throws IOException {
+        contactList.remove(contact);
+        favoriteList.remove(contact);
+        saveContacts();
+        saveFavorites();
+    }
+
+    /**
+     * @brief Aggiunge un contatto ai preferiti.
+     */
+    public void addToFavorites(Contatto contact) throws IOException {
+        if (contactList.contains(contact) && !favoriteList.contains(contact)) {
+            favoriteList.add(contact);
+            saveFavorites();
+        }
+    }
+
+    /**
+     * @brief Rimuove un contatto dai preferiti.
+     */
+    public void removeFromFavorites(Contatto contact) throws IOException {
+        favoriteList.remove(contact);
+        saveFavorites();
+    }
+
+    /**
+     * @brief Cerca contatti nella rubrica principale.
+     */
     public ObservableList<Contatto> searchContact(String param) {
         ObservableList<Contatto> result = FXCollections.observableArrayList();
         for (Contatto contact : contactList) {
@@ -120,16 +137,19 @@ public class Rubrica implements GestioneRubrica {
             }
         }
         return result;
-
     }
+
     /**
-     * @brief Salva i contatti sul file JSON.
-     * 
-     * @throws IOException Se si verifica un errore durante il salvataggio.
+     * @brief Salva i contatti principali sul file JSON.
      */
     private void saveContacts() throws IOException {
         SalvaCaricaRubrica.saveAddressBook(contactList);
     }
-    
-    
+
+    /**
+     * @brief Salva i contatti preferiti sul file JSON.
+     */
+    private void saveFavorites() throws IOException {
+        SalvaCaricaPreferiti.saveFavoritesAddressBook(favoriteList);
+    }
 }
