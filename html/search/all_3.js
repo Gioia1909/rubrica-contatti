c450b6b9bc8c5418873103786f01fe286da819a1
ariannapal <ari19arianna@gmail.com>
2024-12-16 12:47:24 +0100

    Delete html directory

--- html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['e_20documentazione_20di_20progettazione_0',['Specifica dei Requisiti del Software (SRS) e Documentazione di Progettazione',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]]
-];
--- html/search/all_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['team_0',['Team',['../md__r_e_a_d_m_e.html#autotoc_md1',1,'']]]
-];
