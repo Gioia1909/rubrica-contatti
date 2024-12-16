f355eb39edb36466bf1d9bee3eeea15a7484db67
ariannapal <ari19arianna@gmail.com>
2024-12-16 12:32:44 +0100

    Delete html directory

--- html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ingegneria_20del_20software_20gruppo_2009_0',['Rubrica Contatti - Progetto di Ingegneria del Software - Gruppo 09',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- html/search/all_8.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['software_20gruppo_2009_0',['Rubrica Contatti - Progetto di Ingegneria del Software - Gruppo 09',['../md__r_e_a_d_m_e.html',1,'']]],
-  ['software_20srs_20e_20documentazione_20di_20progettazione_1',['Specifica dei Requisiti del Software (SRS) e Documentazione di Progettazione',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]],
-  ['specifica_20dei_20requisiti_20del_20software_20srs_20e_20documentazione_20di_20progettazione_2',['Specifica dei Requisiti del Software (SRS) e Documentazione di Progettazione',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]],
-  ['srs_20e_20documentazione_20di_20progettazione_3',['Specifica dei Requisiti del Software (SRS) e Documentazione di Progettazione',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]]
-];
